ffb1f9508c6ad292fb0b4612b2f0636336a52ee9
Sébastien de Beauffort <sewelseb@hotmail.com>
2017-11-02 22:49:23 +0100

    config

--- Model/connexionDB.php
+++ Model/connexionDB.php
@@ -1,3 +1,3 @@
 <?php
-    //$bdd = new PDO('mysql:host=localhost;dbname=shelby', 'seb', 'seb'); //prod
-    $bdd = new PDO('mysql:host=localhost;dbname=shelby', 'root', 'root'); //dev
+    $bdd = new PDO('mysql:host=localhost;dbname=shelby', 'seb', 'seb'); //prod
+    //$bdd = new PDO('mysql:host=localhost;dbname=shelby', 'root', 'root'); //dev
